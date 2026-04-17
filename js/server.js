90545b3c18be88c236fabb92cfb2648910c3d6d5
gabiemisiqueira-code <gabiemi.siqueira@gmail.com>
2026-04-17 12:15:23 -0300

    Atualizar o server.js

--- js/server.js
+++ js/server.js
@@ -3,6 +3,38 @@ const path = require('path');
 
 const app = express();
 
-// arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static
+app.use(express.static(path.join(__dirname, 'js')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'ranking.html'));
+});
+
+app.get('/ranking', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'ranking.html'));
+});
+
+app.get('/champions', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'champions.html'));
+});
+
+app.get('/player', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'player.html'));
+});
+
+app.get('/compare', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'compare.html'));
+});
+
+app.get('/guides', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'guides.html'));
+});
+
+app.get('/pro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'pro.html'));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`
