c163c4159ae70b686813ff561425485aa7ce67cf
gabiemisiqueira-code <gabiemi.siqueira@gmail.com>
2026-04-17 11:18:14 -0300

    Atualizar o server.js

--- js/server.js
+++ js/server.js
@@ -1,54 +1,41 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+const express = require('express');
+const path = require('path');
 
 const app = express();
-app.use(cors());
 
-const API_KEY = process.env.RIOT_API_KEY;
-const PORT = process.env.PORT || 3000;
+// liberar arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'js')));
 
-app.get("/", (req, res) => {
-  res.send("Servidor Riot funcionando");
+// rotas das páginas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'ranking.html'));
 });
 
-app.get("/player/:gameName/:tagLine", async (req, res) => {
-  try {
-    const { gameName, tagLine } = req.params;
-
-    const accountResponse = await axios.get(
-      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}`,
-      {
-        headers: {
-          "X-Riot-Token": API_KEY
-        }
-      }
-    );
-
-    const puuid = accountResponse.data.puuid;
-
-    const summonerResponse = await axios.get(
-      `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
-      {
-        headers: {
-          "X-Riot-Token": API_KEY
-        }
-      }
-    );
-
-    res.json({
-      gameName,
-      tagLine,
-      ...summonerResponse.data
-    });
-  } catch (error) {
-    res.status(error.response?.status || 500).json({
-      erro: "Erro ao buscar jogador",
-      detalhe: error.response?.data || error.message
-    });
-  }
+app.get('/champions', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'champions.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.get('/player', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'player.html'));
+});
+
+app.get('/compare', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'compare.html'));
+});
+
+app.get('/guides', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'guides.html'));
+});
+
+app.get('/pro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'pro.html'));
+});
+
+app.get('/ranking', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'ranking.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
 });
